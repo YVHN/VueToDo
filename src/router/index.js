55aba128411d0e374388dc9857c591add26675c5
Yevhen Suprunenko <gollandolion@gmail.com>
2023-12-28 10:39:38 +0100

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginUser from '../views/LoginUser.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: LoginUser
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
@@ -22,4 +14,4 @@ const router = createRouter({
   routes
 })
 
-export default router
+export default router;
---
+++ src/views/LoginUser.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return {
+
+            }
+        },
+
+    }
+</script>
+
+<style lang="scss">
+    $main-color1 : rgb(65, 184, 131);
+    $main-color2 : rgb(53, 73, 94);
+</style>
